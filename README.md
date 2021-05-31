95fe28e76bc4722f8512690983f7321394e3b240
brenoh1ldebr4nd <breno.h1ldebr4nd@gmail.com>
2021-05-31 13:34:35 -0400

    auth-nodejs v1.0.0

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1>auth-nodejs</h1>
+
+<p align="justify"><b>Auth-nodejs</b> is a mini project, intended to sharp authentication skills of programmers. It uses <a href="https://github.com/Level/level">level</a> as the database and the console as the interface. The code should be clean, so it's easy to read and understand. Check it out!</p>
+
+<br/>
+
+```bash
+$ git clone https://github.com/brenoh1ldebr4nd/auth-nodejs
+```
+
+```bash
+$ cd auth-nodejs
+$ npm install
+```
+
+```bash
+$ node main.js
+```
+
+<br/>
+
+Here is how it looks like:
+
+<img src="auth-nodejs-screenshot.png" alt="auth-nodejs screenshot">
+
+This project is under the <a href="./LICENSE">MIT</a> license.
---
+++ main.js
@@ -0,0 +1,79 @@
+import level from 'level';
+import promptSync from 'prompt-sync';
+import chalk from 'chalk';
+
+const prompt = promptSync();
+const version = '1.0.0';
+
+async function main() {
+  console.log(chalk.green(`\nAuth v${version}`));
+
+  global.db = level('./database', (err) => {
+    if (err) {
+      console.log(
+        chalk.red(`\nThere was an error while initializing the database.\n`),
+      );
+      throw err;
+    }
+  });
+
+  while (true) {
+    await chooseNextAction();
+  }
+}
+
+async function chooseNextAction() {
+  console.log(chalk.blue('\nChoose an action ( signup, signin, exit )'));
+  const action = prompt('');
+
+  switch (action) {
+    case 'signup':
+      await signUpAction();
+      break;
+    case 'signin':
+      await signInAction();
+      break;
+    case 'exit':
+      process.exit();
+      break;
+    default:
+      console.log(chalk.red('Action not recognized, please try again!'));
+  }
+}
+
+async function signUpAction() {
+  console.log(chalk.blue('\nSign Up'));
+  const username = prompt('username: ');
+  const password = prompt('password: ');
+
+  if (!username || !password) {
+    console.log(
+      chalk.red(
+        '\nThere was some error while reading the inputs! If you did enter inputs, please check the code!\n',
+      ),
+    );
+    process.exit();
+  }
+  await global.db.put(username, password);
+}
+
+async function signInAction() {
+  console.log(chalk.blue('\nSign In'));
+  const username = prompt('username: ');
+  const password = prompt('password: ');
+
+  if (!username || !password) {
+    console.log(
+      chalk.red(
+        '\nThere was some error while reading the inputs! If you did enter inputs, please check the code!\n',
+      ),
+    );
+    process.exit();
+  }
+
+  (await global.db.get(username).catch(() => undefined)) === password
+    ? console.log(chalk.green('\nUser recognized'))
+    : console.log(chalk.red('\nUser not recognized'));
+}
+
+main();
